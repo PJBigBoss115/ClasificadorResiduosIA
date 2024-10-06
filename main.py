c57c25bc8e789d476d1c6354c0170ec717d6860d
Pedro J. Aguilar Vaides <65696918+PJBigBoss115@users.noreply.github.com>
2024-10-05 22:20:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
+import cv2
 from ultralytics import YOLO
 
-# Cargar el modelo YOLOv8 pre-entrenado
-model = YOLO('yolov8n.pt')  # yolov8n es el modelo más ligero
+# Cargar el modelo
+model = YOLO('/runs/detect/train3/weights/best.pt') # Se debe completar la ruta segun donde lo tengas almacenado
 
-# Realizar la detección en una imagen de prueba
-results = model('https://ultralytics.com/images/zidane.jpg')
+# Inicializar la cámara
+cap = cv2.VideoCapture(0) # Por lo general la camara web esta en el 0 pero segun los puetos (USB) puede estar en 3 o 4
 
-# Mostrar los resultados usando plot
-results[0].plot()  # results es una lista, accedemos al primer elemento
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("No se puede recibir frames. Saliendo...")
+        break
+
+    results = model(frame)
+
+    for result in results:
+        if hasattr(result, 'boxes'):
+            detections = result.boxes
+
+            for detection in detections:
+                confidence = detection.conf.item()
+                if confidence > 0.2:
+
+                    startX, startY, endX, endY = map(int, detection.xyxy[0])
+                    label = f"Class {int(detection.cls.item())}: {confidence:.2f}"
+
+                    cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Mostrar las detecciones
+    cv2.imshow("Detecciones en tiempo real", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
